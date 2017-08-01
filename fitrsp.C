d12fa892a7ab6dbcfab14bb1fdefb0088896f06f
sifuluo <sifuluo@tamu.edu>
2017-08-01 16:05:27 -0500

    Added table of summary for parameters

--- fitrsp.C
+++ fitrsp.C
@@ -16,40 +16,18 @@ double dashedfunc(double *xx, double *par)
     (1.0-par[5]) * TMath::Gaus(x,par[1], par[2]*(1+par[6]))    )+par[4]+par[3]-par[4]-par[3];
     return res;
 }
-std::pair<double, double> myfit(const int ieta, const int ipt, double nsigma)
+
+std::vector<double> myfit(string eta1, string eta2, int pt1, int pt2, double nsigma, bool moreinfo)
 {
-  // std::vector<double> etabins = {0., 0.087, 0.174, 0.261, 0.348, 0.435 ,0.522, 0.609, 0.696,
-  // 0.783,  0.879, 0.957, 1.044, 1.131, 1.218, 1.305 ,1.392, 1.479, 1.566,
-  // 1.653,  1.74,  1.83,  1.93,  2.043, 2.172, 2.322 ,2.5,   2.65,  2.853,
-  // 2.964,  3.139, 3.314, 3.489, 3.664, 3.839, 4.013 ,4.191, 4.363, 4.538,
-  // 4.716,4.889,5.191};
-  const string eta_boundaries[] =
-  {"-5.191","-4.889","-4.716","-4.538","-4.363","-4.191","-4.013","-3.839","-3.664","-3.489",
-  "-3.314", "-3.139","-2.964","-2.853","-2.65", "-2.5",  "-2.322","-2.172","-2.043","-1.93",
-  "-1.83",  "-1.74", "-1.653","-1.566","-1.479","-1.392","-1.305","-1.218","-1.131","-1.044",
-  "-0.957", "-0.879","-0.783","-0.696","-0.609","-0.522","-0.435","-0.348","-0.261","-0.174",
-  "-0.087", "0",     "0.087", "0.174", "0.261", "0.348", "0.435" ,"0.522", "0.609", "0.696",
-  "0.783",  "0.879", "0.957", "1.044", "1.131", "1.218", "1.305" ,"1.392", "1.479", "1.566",
-  "1.653",  "1.74",  "1.83",  "1.93",  "2.043", "2.172", "2.322" ,"2.5",   "2.65",  "2.853",
-  "2.964",  "3.139", "3.314", "3.489", "3.664", "3.839", "4.013" ,"4.191", "4.363", "4.538",
-  "4.716","4.889","5.191"};
-
-  std::vector<const int> ptbins =
-  {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 20, 23, 27, 30, 35, 40, 45, 57, 72, 90,
-  120, 150, 200, 300, 400, 550, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 5000};
-
-  const int pt1 = ptbins[ipt];
-  const int pt2 = ptbins[ipt+1];
-
-  string relrspname   = Form("ak4pfchsl1/RelRsp_JetEta%sto%s_RefPt%dto%d",eta_boundaries[ieta+41].c_str(),eta_boundaries[ieta+42].c_str(),pt1,pt2);
-  string jetptname    = Form("ak4pfchsl1/JetPt_JetEta%sto%s_RefPt%dto%d",eta_boundaries[ieta+41].c_str(),eta_boundaries[ieta+42].c_str(),pt1,pt2);
-  cout << relrspname <<endl;
+  string relrspname   = Form("ak4pfchsl1/RelRsp_JetEta%sto%s_RefPt%dto%d",eta1.c_str(),eta2.c_str(),pt1,pt2);
+  string jetptname    = Form("ak4pfchsl1/JetPt_JetEta%sto%s_RefPt%dto%d",eta1.c_str(),eta2.c_str(),pt1,pt2);
+  if (moreinfo) cout << relrspname <<endl;
   TFile *fin = new TFile("jra.root");
   TH1F *h1 = (TH1F*)fin->Get(relrspname.c_str());
   TH1F *h2 = (TH1F*)fin->Get(jetptname.c_str());
   double jetpt = h2->GetMean();
 
-  h1->Rebin(10);
+  // h1->Rebin(10);
 
   TF1* fitfnc(0);
   const double ptmin = pt1;
@@ -66,8 +44,6 @@ std::pair<double, double> myfit(const int ieta, const int ipt, double nsigma)
   mean = p;
   double fitsigma = nsigma * rms;
 
-
-  // double norm = h1->GetMaximumStored();
   struct para {
     const string name; const double ival; const double minval; const double maxval;
   };
@@ -75,22 +51,23 @@ std::pair<double, double> myfit(const int ieta, const int ipt, double nsigma)
     {"norm      ", integr   , 0.                         , 2*integr       },
     {"core_mu   ", mean     , 0.7*mean                  , 1.2*mean       },
     {"core_sigma", rms      , 0.4*rms                    , 1.5*rms        },
-    {"cdf_mu    ", 3.5/ptmin , 3./ptmin                  , 3.5/ptmin       },
+    {"cdf_mu    ", 3.5/ptmin , 2.0/ptmin                  , 5./ptmin       },
     {"cdf_sigma ", 0.2/ptmin, 0.1/ptmin               ,  1./ptmin       },
-    {"core_frac ", 0.9      , min(0.9,(ptmin-2)*0.05+0.4), 1.0            },
-    {"sigma_inc ", 0.5*rms  , 0.02                       , 8.             },
+    {"core_frac ", 0.9      , min(0.4,(ptmin-2)*0.05+0.4), 1.0            },
+    {"sigma_inc ", 0.5  , 0.02                       , 2.             },
   };
 
   std::vector<double> paravals;
   for (int ipush = 0; ipush < paras.size(); ++ipush){
     paravals.push_back(paras[ipush].ival);
   }
-
+  std::vector<double> paralims;
+  paralims.resize(paravals.size(),0);
 
   // double xmin = h1->GetXaxis()->GetXmin();
   double xmin = 0.8/(ptmin+1.);
 
-  if (pt1 > 7) xmin = max(paravals[1]-1.0*rms,xmin);
+  if (pt1 > 7) xmin = max(paravals[1]-0.8*rms,xmin);
   double xmax = min(h1->GetXaxis()->GetXmax(),paravals[1] + 2.0*rms);
 
   for (int iiter = 0; iiter < 15; iiter++){
@@ -103,8 +80,6 @@ std::pair<double, double> myfit(const int ieta, const int ipt, double nsigma)
       fitfnc->SetParLimits(iset, paras[iset].minval, paras[iset].maxval);
     }
     h1->Fit(fitfnc,"RQ0");
-    // delete fitfnc;
-    // fitfnc = h1->GetFunction("multigaus");
 
     for (int iget = 0; iget < paras.size(); ++iget) {
       paravals[iget] = fitfnc->GetParameter(iget);
@@ -116,38 +91,35 @@ std::pair<double, double> myfit(const int ieta, const int ipt, double nsigma)
   h1->Draw();
   fitfnc->Draw("same");
 
-  // bool comparecdf = false;
-  // if (comparecdf){
-  //   TF1* dashline = new TF1("dashed",dashedfunc,xmin,xmax,7);
-  //   dashline->SetParNames("N","Core #mu","Core #sigma","CDF #mu","CDF #sigma","Tail Frac","Tail #sigma");
-  //
-  //   for(int iset = 0; iset < paras.size(); ++iset){
-  //     dashline->SetParameter(iset, paravals[iset]);
-  //   }
-  //   dashline->SetLineColor(3);
-  //   dashline->Draw("same");
-  // }
-  // TH1F *newh1 = (TH1F*)h1->Clone();
-  // for (int ibin = 0; ibin <= h1->GetNbinsX(); ++ibin)
-  // {
-  //   newh1->SetBinContent(ibin, h1->GetBinContent(ibin) / ROOT::Math::normal_cdf(h1->GetBinCenter(ibin),paravals[4],paravals[3]) );
-  //   newh1->SetBinError(ibin, h1->GetBinError(ibin) / ROOT::Math::normal_cdf(h1->GetBinCenter(ibin),paravals[4],paravals[3]) );
-  // }
-
-  //median
-  // double medianx, medianq;
-  // medianq = 0.5;
-  // h1->GetQuantiles(1, &medianx, &medianq);
-  // TLine *l1 = new TLine(medianx,0,medianx,h1->GetMaximum());
-  // l1->Draw("same");
-  //
-  // double medianx2;
-  // newh1->GetQuantiles(1, &medianx2, &medianq);
-  // TLine *newl1 = new TLine(medianx2,0,medianx2,h1->GetMaximum());
-  // newl1->SetLineColor(4);
-  // newl1->Draw("same");
-
   jetpt *= paravals[1]/h1->GetMean();
+  double chi2 = fitfnc->GetChisquare();
+  double ndf  = fitfnc->GetNDF();
+  for (int iprint = 0; iprint < paras.size(); ++iprint){
+    if(paravals[iprint] < paras[iprint].minval * 1.01) {
+      paralims[iprint] = 1;
+    }
+    if(paravals[iprint] > paras[iprint].maxval * 0.99) {
+      paralims[iprint] = 2;
+    }
+  }
+  //compose the output values
+  std::vector<double> output;
+  for (int iout = 0; iout < paravals.size(); ++iout) {
+    output.push_back(paravals[iout]);// (ipara-1)*5
+    output.push_back(paras[iout].ival); // (ipara -1)*5 +1
+    output.push_back(paras[iout].minval);// (ipara -1)*5 +2
+    output.push_back(paras[iout].maxval); //(ipara -1)*5 +3
+    output.push_back(paralims[iout]);      //(ipara -1)*5 +4
+  }
+  output.push_back(chi2);   // npara * 5
+  output.push_back(ndf);    // npara * 5 + 1
+  output.push_back(jetpt);  // npara * 5 + 2
+  output.push_back((double)pt1);    // npara * 5 + 3
+  output.push_back(pt2);            // npara * 5 + 4
+
+  //needed output all set
+
+  if(!moreinfo) return output;
 
   for (int iprint = 0; iprint < paras.size(); ++iprint){
     cout << Form("%s = %10.5g => %10.5g With Limits [ %10.5g , %10.5g ]",paras[iprint].name.c_str(), paras[iprint].ival, paravals[iprint], paras[iprint].minval, paras[iprint].maxval) << endl;
@@ -155,8 +127,7 @@ std::pair<double, double> myfit(const int ieta, const int ipt, double nsigma)
     if(paravals[iprint] > paras[iprint].maxval * 0.99) cout << "*********Hitting the UPPER limit*********" <<endl;
   }
   cout << "Threshold = " << paravals[3]*pt1 <<endl;
-  double chi2 = fitfnc->GetChisquare();
-  double ndf  = fitfnc->GetNDF();
+
   // cout << "median = " << medianx <<endl;
   // cout << "median without Threshold Effect = " << medianx2 <<endl;
   cout << "Chi Square / NDF = "  << chi2 <<"/"<< ndf <<" = "<<chi2 / ndf<< endl;
@@ -165,27 +136,77 @@ std::pair<double, double> myfit(const int ieta, const int ipt, double nsigma)
   cout << endl;
 
   // std::pair<double, double> corrdots = std::make_pair(jetpt,1. / paravals[1]);
-  std::pair<double, double> corrdots = std::make_pair(jetpt,1. / paravals[1]);
+  // std::pair<double, double> corrdots = std::make_pair(jetpt,1. / paravals[1]);
 
-  return corrdots;
+
+
+  return output;
 
 }
 
-void fitit(const int ieta = 0,const double nsigma = 1.5)
+void fitrsp(const int ieta = 0,const double nsigma = 1.5)
 {
-  cout << "=============================================================================================================="<< endl;
+  cout << "============================================================================================================================================"<< endl;
+  const string eta_boundaries[] =
+  {"-5.191","-4.889","-4.716","-4.538","-4.363","-4.191","-4.013","-3.839","-3.664","-3.489",
+  "-3.314", "-3.139","-2.964","-2.853","-2.65", "-2.5",  "-2.322","-2.172","-2.043","-1.93",
+  "-1.83",  "-1.74", "-1.653","-1.566","-1.479","-1.392","-1.305","-1.218","-1.131","-1.044",
+  "-0.957", "-0.879","-0.783","-0.696","-0.609","-0.522","-0.435","-0.348","-0.261","-0.174",
+  "-0.087", "0",     "0.087", "0.174", "0.261", "0.348", "0.435" ,"0.522", "0.609", "0.696",
+  "0.783",  "0.879", "0.957", "1.044", "1.131", "1.218", "1.305" ,"1.392", "1.479", "1.566",
+  "1.653",  "1.74",  "1.83",  "1.93",  "2.043", "2.172", "2.322" ,"2.5",   "2.65",  "2.853",
+  "2.964",  "3.139", "3.314", "3.489", "3.664", "3.839", "4.013" ,"4.191", "4.363", "4.538",
+  "4.716","4.889","5.191"};
+  std::vector<const int> ptbins =
+  {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 20, 23, 27, 30, 35, 40, 45, 57, 72, 90,
+  120, 150, 200, 300, 400, 550, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 5000};
+
   TCanvas *c1 = new TCanvas("c1","c1",3000,1500);
-  int xpad = 3, ypad = 2;
+  int xpad = 5, ypad = 4;
   c1->Divide(xpad, ypad);
+  bool moreinfo = 0;
 
   TGraph *gr = new TGraph();
-  for (int ipad = 1; ipad <= xpad* ypad; ++ipad ){
-    c1->cd(ipad);
-    pair<double, double> corrdots = myfit(ieta, ipad - 1, nsigma);
-    // gr->SetPoint(gr->GetN(),ipad+2, 1 / myfit(ieta, ipad, nsigma);
-    gr->SetPoint(gr->GetN(),corrdots.first,corrdots.second);
+  std::vector<std::vector<double> > outputs;
+  string eta1 = eta_boundaries[ieta + 41];
+  string eta2 = eta_boundaries[ieta + 42];
+  int npara = 7;
 
+  for (int ipad = 1; ipad <= xpad* ypad; ++ipad ){
+    int ipt = ipad - 1;
+    int pt1 = ptbins[ipt];
+    int pt2 = ptbins[ipt+1];
+
+    c1->cd(ipad);
+    outputs.push_back(myfit(eta1,eta2,pt1,pt2,nsigma,moreinfo));
+    // gr->SetPoint(gr->GetN(),ipad+2, 1 / myfit(ieta, ipad, nsigma);
   }
+  if (moreinfo){
+    cout <<endl;
+    cout << "---------------------------------------------------------------------------------------------------------------------" <<endl<<endl;
+  }
+  cout << "Summary on Response of eta Range = [ " << eta1 << " , " << eta2 << " ]" <<endl;
+  cout << "  RefPt"<<"       Norm "<<"     core_mu "<<"  core_sigma "<<"      cdf_mu "<<"   cdf_sigma "<<"   core_frac "<<"   sigma_inc " <<"    Chi2/NDF " <<" Threshold"<<endl;
+
+
+  for (int iout = 0; iout < outputs.size(); ++iout){
+    std::vector<double> printvec = outputs.at(iout);
+    cout << Form("%3d-%3d ",int(printvec.at(npara*5+3)),int(printvec.at(npara*5+4)));
+    for (int iout1 = 0; iout1 < npara; ++iout1){
+      string limflag = " ";
+      if (printvec.at(iout1*5+4) == 1) limflag = "L";
+      if (printvec.at(iout1*5+4) == 2) limflag = "H";
+      cout << Form("%10.3f %s ",printvec.at(iout1*5),limflag.c_str());
+    }
+    cout << Form("%10.3f %10.3f",printvec.at(npara*5) / printvec.at(npara*5+1), printvec.at(3*5) * printvec.at(npara*5+3));
+    cout << endl;
+    if (moreinfo) cout <<endl;
+
+    double xaxis = printvec.at(npara*5+2);
+    double yaxis = 1/printvec.at(1*5);
+    gr->SetPoint(gr->GetN(),xaxis,yaxis);
+  }
+
   TCanvas *c2 = new TCanvas("c2","c2",800,600);
   c2->cd();
   gr->Draw("*al");
